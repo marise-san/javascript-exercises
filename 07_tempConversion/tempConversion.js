73c8656b0bbb53984e2f588d2e6fb2af8102f9d4
marise-san <marisemrtns@gmail.com>
2022-06-09 10:02:31 -0300

    Update code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
+const ftoc = (fah) => {
+  const cel = (fah - 32) / 1.8;
+  const rounded = (Math.round(cel *10) / 10);
+  
+  return rounded;
+} 
 
-};
+const ctof = (cel) => {
+  const fah = 1.8 * cel + 32;
+  const rounded = Math.round(fah *10) / 10;
 
-const ctof = function() {
-
-};
+  return rounded;
+}
 
 // Do not edit below this line
 module.exports = {
