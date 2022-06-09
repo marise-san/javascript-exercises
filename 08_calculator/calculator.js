d8b2baaaed9f52ee7863f01a0dd4079abad44ba0
marise-san <marisemrtns@gmail.com>
2022-06-09 12:41:52 -0300

    Update code

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	const adds = num1 + num2;
+  return adds;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  const sub = num1 - num2;
+  return sub;	
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  let sumArray = 0;
+  for(i = 0; i < args.length; i++){
+    sumArray += args[i];
+  }
+  return sumArray;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let multiplied = 1;
+  for(i = 0; i < args.length; i++){
+    multiplied *= args[i]; 
+  }
+  return multiplied;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let powered = 1;
+  for(let i = 0; i < num2; i++){
+    powered *= num1;
+  }
+  return powered;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  fac = num;
+  result = fac;
+  if(fac === 0){
+    return 1;
+  } else {
+  for(let i = 1; i < fac; i++){
+    result *= i;
+  }
+}
+  return result;
 };
 
 // Do not edit below this line
