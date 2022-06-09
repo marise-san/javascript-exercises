0a958201a1e1944de0c3af45a4ac15f45d74642a
marise-san <marisemrtns@gmail.com>
2022-06-08 21:51:03 -0300

    Update code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = (ini, end) => {
+    
+    if (ini < 0 || typeof(end) === 'string' || Array.isArray(end)) {
+        return "ERROR";
+    } else {
+        let total = 0;
+        if (ini < end) {
+            for (let i = ini; i <= end; i++) {
+                total = total + i;
+            }
+        } else {
+            for (let i = end; i <= ini; i++) {
+                total = total + i;
+            }
+        }
+        return total;
+    }
+}
 
-};
-
+sumAll(1, 4);
 // Do not edit below this line
 module.exports = sumAll;
