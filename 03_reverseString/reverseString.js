916a20e09ee6001e643f50727c23cb510f46826f
marise-san <marisemrtns@gmail.com>
2022-06-08 21:02:48 -0300

    Update code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = (string) => {
+    let reversedString = '';
+    for(let i = string.length - 1; i >= 0; i--){
+        reversedString += string[i];
+    }
+    return reversedString;
+}
 
-};
-
+reverseString('hello there');
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+function removeFromArray(){
+    const args = Array.from(arguments);
+    const arrFromArgs = args[0];
 
-};
+    for(i = 1; i < args.length; i++){
+        for(j = 0; j < arrFromArgs.length; j++){
+            if(arrFromArgs[j] === args[i]){
+                arrFromArgs.splice(j, 1);
+            }
+        }
+    }
+    return arrFromArgs;
+}
 
+console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
