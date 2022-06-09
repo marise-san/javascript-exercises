d4cb827bccd138844791a3ec53981d84d74aa321
marise-san <marisemrtns@gmail.com>
2022-06-09 17:38:26 -0300

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (arrPosition) {
+    if (arrPosition <= 0) {
+        return "OOPS";
+    } else {
+        let fibo = [];
+        fibo[0] = 1;
+        fibo[1] = 1;
+        for (let i = 2; i < arrPosition; i++) {
+            fibo[i] = fibo[i - 2] + fibo[i - 1];
+        }
+        return fibo.pop();
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
