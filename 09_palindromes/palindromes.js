a37d802201bff1ad6a95aa655304ae54008bf813
marise-san <marisemrtns@gmail.com>
2022-06-09 15:54:33 -0300

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const arr = string.split('');
+    const filtered = arr.filter(catchSym = (arr) => {
+        return arr !== " " &&
+            arr !== "," &&
+            arr !== "." &&
+            arr !== "!";
+    });
 
-};
+    const str = filtered.join("").toLowerCase();
+    const revStr = filtered.reverse().join("").toLowerCase();
+
+    const palindrome = (str === revStr) ? true : false;
+
+    return palindrome;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
