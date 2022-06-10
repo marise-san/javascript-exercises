39e45509bc62fb8301fa5d36301e3b143cae1cd1
marise-san <marisemrtns@gmail.com>
2022-06-10 11:15:40 -0300

    Complete the test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
+const getTheTitles = function () {
+    const books = [
+        { title: 'Book', author: 'Name' },
+        { title: 'Book2', author: 'Name2' }
+    ];
 
+    const onlyTitle = books.map(
+        function (item, index) {
+            return item.title;
+        });
+    return onlyTitle;
 };
+
+
+console.log(getTheTitles());
 
 // Do not edit below this line
 module.exports = getTheTitles;
